e030d96da183fb5d0dd3cf0a6d465361100bcb8f
Bknibb <bailey.knibb@gmail.com>
2025-08-08 19:57:56 +1000

    add github assembly attribute

--- RoundsWithFriends/AssemblyInfo.cs
+++ RoundsWithFriends/AssemblyInfo.cs
@@ -1,5 +1,21 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 
 [assembly: AssemblyVersion(RWF.RWFMod.Version)]
 [assembly: AssemblyFileVersion(RWF.RWFMod.Version)]
 [assembly: AssemblyInformationalVersion(RWF.RWFMod.Version)]
+
+[assembly: Github("Bknibb", "RoundsWithFriends")]
+
+[AttributeUsage(AttributeTargets.Assembly)]
+public class GithubAttribute : Attribute
+{
+    public string Owner { get; }
+    public string Repo { get; }
+
+    public GithubAttribute(string owner, string repo)
+    {
+        Owner = owner;
+        Repo = repo;
+    }
+}
